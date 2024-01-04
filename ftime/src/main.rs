3806ed732c24c37421f3a2f657c038ba699a53c2
Boris Uskov <uskov_b@mail.ru>
2024-01-04 10:28:58 +0300

    Add doc comments

--- ftime/src/main.rs
+++ ftime/src/main.rs
@@ -11,10 +11,14 @@ const FERRIS: &str = r#"
   / '-----' \
 "#;
 
+/// X coordinate for time indication
 const TIME_X_OFFSET: u32 = 5;
+
+/// Y coordinate for time indication
 const TIME_Y_OFFSET: u32 = 1;
 
 fn main() {
+    // Remove program artefact from the screen and exit correct
     ctrlc::set_handler(move || {
         clear();
         process::exit(0)
@@ -34,23 +38,64 @@ fn main() {
     }
 }
 
+/// Prints time in HH:MM:SS format
+/// 
+/// # Usage
+/// 
+/// ```rust
+/// print_time();
+/// ```
+/// 
+/// # Example
+/// 09:59:17
 fn print_time() {
     let local: DateTime<Local> = Local::now();
     println!("{}", local.format("%H:%M:%S").blue());
 }
 
+/// Prints Ferris crab on the screen
+/// 
+/// # Usage
+/// 
+/// ```rust
+/// print_ferris();
+/// ```
 fn print_ferris() {
     print!("{}", FERRIS.yellow());
 }
 
+/// Clears the screen
+/// 
+/// # Usage
+/// 
+/// ```rust
+/// clear();
+/// ```
 fn clear() {
     print!("\x1b[2J");
 }
 
+/// Hides cursor
+/// 
+/// # Usage
+/// 
+/// ```rust
+/// hide_cursor();
+/// ```
 fn hide_cursor() {
     print!("\x1b[?25l");
 }
 
+/// Moves cursor to point (x,y)
+/// # Params
+/// `x` column
+/// 'y' row
+/// 
+/// # Usage
+/// 
+/// ```rust
+/// movexy(10, 15);
+/// ```
 fn movexy(x: u32, y: u32) {
     print!("\x1b[{y};{x}H");
 }
